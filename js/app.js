09e7313cb3c379184fe1138ff714bf2431a064a8
hermesalexis <hermesagm@gmail.com>
2017-02-28 21:39:01 -0500

    generate aleatory number

--- js/app.js
+++ js/app.js
@@ -1,9 +1,48 @@
+class Number {
 
-var add = (function (){
-  var counter = 0;
-  return function(){return counter +=1;}
-})();
+  constructor(minGoalNumber, maxGoalNumber){
+    this.minAleatoryNumber = minGoalNumber;
+    this.maxAleatoryNumber = maxGoalNumber;
+    this.numberGoal = function(){
+      return generateValidNumber(this.minAleatoryNumber, this.maxAleatoryNumber);
+    }
+    this.spades = 0;
+    this.fixed = 0;
 
 
-console.log(add());
-console.log(add());
+    var countDigitInNumber = function(element, number){ // encuentra un digito en el número y cuento cuantas veces esta
+      let regExp = new RegExp(element, "g");
+      return this.match(regExp).length;
+    }
+
+    var  generateTimesDigits= function(number){ //cuenta el número de veces qur esta cada digito un dígito
+      let stringNumber =  number.toString();
+      let arrayNumber = stringNumber.split("");
+      return arrayNumber.map(countDigitInNumber, stringNumber);
+    }
+
+    var generateNumber = function(minNumber, maxNumber){ // genera un número aleatorio en el rango indicado
+      let range = (maxNumber - minNumber + 1);
+      return (Math.floor(Math.random() * range)  + minNumber);
+    }
+
+    var generateValidNumber = function(minNumber, maxNumber){ //genera un número aleatorio que no tenga digitos repetidos
+       let numberOk= false;
+
+       while(!numberOk){
+         var number = generateNumber(minNumber, maxNumber);
+         var timesDigits = generateTimesDigits(number);
+         let index = 0;
+         while(timesDigits[index] === 1 && index < timesDigits.length){index++;}
+         if(index === timesDigits.length){numberOk = true;}
+       }
+
+       return number;
+    }
+
+  }
+}
+
+
+var obj1 = new Number(100000,999999);
+console.log(obj1.numberGoal());
