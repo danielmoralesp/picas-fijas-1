74e3c19bbfb2f8b1783e43e49c2bad171191a86c
hermesalexis <hermesagm@gmail.com>
2017-03-01 12:29:20 -0500

    implement method for cheack a valid number

--- js/app.js
+++ js/app.js
@@ -3,57 +3,60 @@ class Number {
   constructor(minGoalNumber, maxGoalNumber){
     this.minAleatoryNumber = minGoalNumber;
     this.maxAleatoryNumber = maxGoalNumber;
+    this.spades = 0;
+    this.fixed = 0;
     this.goalNumber = 0;
     this.inputNumber = 0;
-    this.numberSpades = 0;
-    this.numberFixed = 0;
-  }
 
+    var  countTimesDigits= function(number){ //cuenta el número de veces qur esta cada digito un dígito
+      var countDigitInNumber = function(element, number){ // encuentra un digito en el número y cuento cuantas veces esta
+        let regExp = new RegExp(element, "g");
+        return this.match(regExp).length;
+      }
 
-  generateGoalNumber(){
-
-    var countDigitInNumber = function(element, number){ // encuentra un digito en el número y cuento cuantas veces esta
-      let regExp = new RegExp(element, "g");
-      return this.match(regExp).length;
-    }
-
-    var  generateTimesDigits= function(number){ //cuenta el número de veces qur esta cada digito un dígito
       let stringNumber =  number.toString();
       let arrayNumber = stringNumber.split("");
       return arrayNumber.map(countDigitInNumber, stringNumber);
     }
 
-    var generateNumber = function(minNumber, maxNumber){ // genera un número aleatorio en el rango indicado
-      let range = (maxNumber - minNumber + 1);
-      return (Math.floor(Math.random() * range)  + minNumber);
+
+    var isValidNumber = function(number){
+      let timesDigits = countTimesDigits(number);
+      let index = 0;
+      while(timesDigits[index] === 1 && index < timesDigits.length){index++;}
+      var numberOk = index === timesDigits.length ? true : false;
+      return numberOk;
     }
 
-    var generateValidNumber = function(minNumber, maxNumber){ //genera un número aleatorio que no tenga digitos repetidos
-       let numberOk= false;
+    this.setGoalNumber = function(){
 
-       while(!numberOk){
-         var number = generateNumber(minNumber, maxNumber);
-         let timesDigits = generateTimesDigits(number);
-         let index = 0;
-         while(timesDigits[index] === 1 && index < timesDigits.length){index++;}
-         if(index === timesDigits.length){numberOk = true;}
-       }
+      var generateNumber = function(minNumber, maxNumber){ // genera un número aleatorio en el rango indicado
+        let range = (maxNumber - minNumber + 1);
+        return (Math.floor(Math.random() * range)  + minNumber);
+      }
 
-       return number;
+      var generateValidNumber = function(minNumber, maxNumber){ //genera un número aleatorio que no tenga digitos repetidos
+         let numberOk= false;
+
+         while(!numberOk){
+           var number = generateNumber(minNumber, maxNumber);
+           let timesDigits = countTimesDigits(number);
+           let index = 0;
+           while(timesDigits[index] === 1 && index < timesDigits.length){index++;}
+           if(index === timesDigits.length){numberOk = true;}
+         }
+
+         return number;
+      }
+      this.goalNumber =  generateValidNumber(this.minAleatoryNumber, this.maxAleatoryNumber);
     }
-    this.goalNumber =  generateValidNumber(this.minAleatoryNumber, this.maxAleatoryNumber);
+
+
   }
 
 }
 
 
 var obj1 = new Number(10,100);
-var obj2 = new Number(1000, 10000);
-console.log(obj1.goalNumber);
-console.log(obj2.goalNumber);
-obj1.generateGoalNumber();
-obj2.generateGoalNumber();
-
-console.log(obj1.goalNumber);
-console.log(obj2.goalNumber);
-console.log(obj2);
+obj1.setGoalNumber();
+console.log(obj1.goalNumber)
