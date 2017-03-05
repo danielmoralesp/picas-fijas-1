21f5a0670eede79e46064a26dce5df410af24516
hermesalexis <hermesagm@gmail.com>
2017-03-05 16:30:14 -0500

    implements class Number

--- js/app.js
+++ js/app.js
@@ -1,17 +1,18 @@
+/////////////// 'Class' Number (Model)/////////////////////////
 
-  var Number = function(){
+  var Number = (function(){
     var minAleatoryNumber = 0;
     var maxAleatoryNumber = 0;
     var goalNumber = 0;
     var inputNumber = 0;
     var elements = {numberSpades: 0,numberFixeds:0};
     
-    var generateNumber = function(minNumber, maxNumber){ // genera un número aleatorio en el rango indicado
+    function generateNumber (minNumber, maxNumber){ // genera un número aleatorio en el rango indicado
       let range = (maxNumber - minNumber + 1);
       return (Math.floor(Math.random() * range)  + minNumber);
     }
     
-    var generateValidNumber = function(minNumber, maxNumber){ //genera un número aleatorio que no tenga digitos repetidos
+    function generateValidNumber (minNumber, maxNumber){ //genera un número aleatorio que no tenga digitos repetidos
        var number = generateNumber(minNumber, maxNumber);
 
        while(!isValidNumber(number)){
@@ -21,9 +22,9 @@
        return number;
     }
     
-    var  countTimesDigits= function(number){ //cuenta el número de veces qur esta cada digito en un número
+    function countTimesDigits (number){ //cuenta el número de veces qur esta cada digito en un número
 
-      var countDigitInNumber = function(element, number){ // encuentra un digito en el número y cuento cuantas veces esta
+      function countDigitInNumber (element, number){ // encuentra un digito en el número y cuento cuantas veces esta
         let regExp = new RegExp(element, "g");
         return this.match(regExp).length; // this se refiere al tercer elemento que se envia al invocar la funcion
       }
@@ -33,7 +34,7 @@
       return arrayNumber.map(countDigitInNumber, stringNumber);
     }
 
-    var isValidNumber = function(number){ // verifica que un número no tenga digitos repetidos
+    function isValidNumber (number){ // verifica que un número no tenga digitos repetidos
       let index = 0;
       let timesDigits = countTimesDigits(number);
       while(timesDigits[index] === 1 && index < timesDigits.length){index++;}
@@ -42,7 +43,7 @@
       return numberOk;
     }
 
-    var checkValidInputNumber = function(){
+    function checkValidInputNumber (){
       var lenGoalNumber = goalNumber.split("").length;
       var regExp = new RegExp("\\b" + "\\d{" + lenGoalNumber + "}" +"\\b");
       var validNumber = false;
@@ -54,17 +55,17 @@
       return validNumber
     }
 
-    var getPositionDigit = function(digit){ // retorna la posicion en la que se encuentra el digito en el el número generado por el sistema
+    function getPositionDigit (digit){ // retorna la posicion en la que se encuentra el digito en el el número generado por el sistema
       var regExp = new RegExp(digit);
       return goalNumber.match(regExp).index
     }
 
-    var isDigitInNumber = function(digit){ // verifica que el digito esta en el número generado por el sistema
+    function isDigitInNumber (digit){ // verifica que el digito esta en el número generado por el sistema
       var regExp = new RegExp(digit);
       return goalNumber.toString().match(regExp) != null ? true : false
     }
 
-    var matchDigitInGoalNumber = function(item, index){ // verifica que un digito del numero que ingresado este en la misma que el número generado por el sistema
+    function matchDigitInGoalNumber (item, index){ // verifica que un digito del numero que ingresado este en la misma que el número generado por el sistema
 
       if(isDigitInNumber(item)){ // verifica que si este el digito en el número
         var match = getPositionDigit(item) === index ? "fixed" : "spade"; // fijas verdadero, picas falso
@@ -72,7 +73,7 @@
       return match || "NA"; //si no hay digitos en el número no aplica
     }
 
-    var countElements = function(typeElement){
+    function countElements (typeElement){
       return function(){ // se aprovecha del clousure
         var arrayInputNumber = inputNumber.split("");
         var contElement = 0;
@@ -87,40 +88,122 @@
 
     }
 
-    return {
-      setMinGoalNumber: function(minGoalNumber){minAleatoryNumber = minGoalNumber;},
-      setMaxGoalNumber: function(maxGoalNumber){maxAleatoryNumber = maxGoalNumber;},
-      setGoalNumber: function(){goalNumber =  generateValidNumber(minAleatoryNumber, maxAleatoryNumber).toString();},
-      getGoalNumber: function(){return goalNumber;},
-      setInputNumber: function(inputN){inputNumber = inputN.toString();},
-      getInputNumber: function(){return inputNumber;},
-      isValidInputNumber: function(){ return checkValidInputNumber();}, // verifica que el número ingresado por el usuario sea válido
-      setSpades : function(){
-        var countSpades = countElements("spade");
-        elements.numberSpades = countSpades();
-      },
-      getSpades: function(){return elements.numberSpades;},
-      setFixeds :function(){
-        var countFixeds = countElements("fixed");
-        elements.numberFixeds = countFixeds();
-      },
-      getFixeds: function(){return elements.numberFixeds;}
+    function setMinGoalNumber (minGoalNumber){minAleatoryNumber = minGoalNumber;}
+
+    function setMaxGoalNumber(maxGoalNumber){maxAleatoryNumber = maxGoalNumber;}
+
+    function setGoalNumber(){goalNumber =  generateValidNumber(minAleatoryNumber, maxAleatoryNumber).toString();}
+
+    function getGoalNumber(){return goalNumber;}
+
+    function setInputNumber(inputN){inputNumber = inputN.toString();}
+
+    function getInputNumber(){return inputNumber;}
+
+    function getSpades(){return elements.numberSpades;}
+
+    function getLengGoalNumber(){return getGoalNumber().length}
+
+    function setSpades(){
+      var countSpades = countElements("spade");
+      elements.numberSpades = countSpades();
     }
 
-  };
+    function getFixeds(){return elements.numberFixeds;}
+
+    function setFixeds(){
+      var countFixeds = countElements("fixed");
+      elements.numberFixeds = countFixeds();
+    }
+
+    function isValidInputNumber(){ return checkValidInputNumber();} // verifica que el número ingresado por el usuario sea válido
+
+    function isWinnerNumber(){
+      return getFixeds() === getGoalNumber().length ? true : false;
+    }
 
 
-var obj1 = new Number();
+    function getActualNumberObject(){
+      var number = {};
+      number.inputNumber = getInputNumber();
+      number.numberSpades = getSpades();
+      number.numberFixeds = getFixeds();
+      return number;
+    }
 
-obj1.setMinGoalNumber(1000);
-obj1.setMaxGoalNumber(9999);
+    function setMinGoalNumber (minGoalNumber){minAleatoryNumber = minGoalNumber;}
 
-obj1.setGoalNumber();
-obj1.setInputNumber(1234);
-console.log(obj1.isValidInputNumber());
-console.log(obj1.getGoalNumber());
-obj1.setSpades();
-obj1.setFixeds();
-console.log("numero de picas: " + obj1.getSpades());
-console.log("numero de fijas: " + obj1.getFixeds());
+    return {
+      setMinGoalNumber:      setMinGoalNumber,
+      setMaxGoalNumber:      setMaxGoalNumber,
+      setGoalNumber:         setGoalNumber,
+      getGoalNumber:         getGoalNumber,
+      setInputNumber:        setInputNumber,
+      getInputNumber:        getInputNumber,
+      isValidInputNumber:    isValidInputNumber, // true valido, false invalido
+      setSpades :            setSpades,
+      getSpades:             getSpades,
+      setFixeds :            setFixeds,
+      getFixeds:             getFixeds ,
+      isWinnerNumber:        isWinnerNumber,
+      getActualNumberObject: getActualNumberObject,
+      getLengGoalNumber:     getLengGoalNumber
+    }
+
+  })();
+
+  /////////////// 'Class' GAME (Controller)/////////////////////////
+
+  function Game(min,max){
+    this.min = min;
+    this.max = max;
+  }
+
+  Game.prototype.startGame = function(){
+    Number.setMinGoalNumber(this.min);
+    Number.setMaxGoalNumber(this.max);
+    Number.setGoalNumber();
+  }
+
+  Game.prototype.endGame = function(){
+    console.log("Felicitaciones has ganado el juego!!!");
+  }
+
+  Game.prototype.Playgame = function(inputNumber){
+    var endGame = false;
+    function showMessageEndGame(){
+      return "Felicitaciones has ganado";
+    }
+
+    Number.setInputNumber(inputNumber);
+
+    if(!Number.isValidInputNumber()){
+      console.log("El número es invalido, debe tener " + Number.getLengGoalNumber() + " Cifras diferentes");
+    }else{
+      Number.setSpades();
+      Number.setFixeds();
+      if(Number.isWinnerNumber()){
+        showMessageEndGame();
+        endGame = true;
+        
+      }else{
+        let number = Number.getActualNumberObject();
+        console.log("Numero: " + number.inputNumber);
+        console.log("Picas: " + number.numberSpades);
+        console.log("Fijas: " + number.numberFixeds);
+      }
+    }
+    return endGame;
+  }
+
+
+  var game = new Game(1000,9999);
+  game.startGame();
+  console.log(Number.getGoalNumber());
+  console.log("Bienvenido al juego de picas y fijas!!!!!! \n\n");
+
+  do{
+   var inputNumber = prompt("Ingresa por favor un numero de 4 cifras");
+  }while(!game.Playgame(inputNumber))
+  game.endGame();
 
